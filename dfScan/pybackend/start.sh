8be8f2b50ad2a63436b2c2ca452174d0860811aa
juzwaaaaaaaa <162950865+juzwaaaaaaaa@users.noreply.github.com>
2025-04-24 16:43:08 +0800

    update(27)

--- dfScan/pybackend/start.sh
+++ dfScan/pybackend/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "✅ Starting Gunicorn..."
+# (optional) Log Tesseract version
+tesseract --version || echo "❌ Tesseract not detected"
+
+# Run Flask app with Gunicorn
 exec gunicorn Scanner:app --bind 0.0.0.0:$PORT
