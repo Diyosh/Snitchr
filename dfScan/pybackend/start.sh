7f1870c4c99cba4b8b95c226a55e75b9b4833002
juzwaaaaaaaa <162950865+juzwaaaaaaaa@users.noreply.github.com>
2025-04-24 15:56:33 +0800

    update(24)

--- dfScan/pybackend/start.sh
+++ dfScan/pybackend/start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# Run your Flask app via Gunicorn
+# Install tesseract if not yet available
+apt-get update && apt-get install -y tesseract-ocr
+
+# Explicitly tell Python where to find tesseract
+export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata
+export PATH=$PATH:/usr/bin
+export TESSERACT_CMD=/usr/bin/tesseract
+
+# Run app with Gunicorn
 exec gunicorn Scanner:app --bind 0.0.0.0:$PORT
