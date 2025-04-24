c2301b51ba2779c5f6cd5ea4a9c0ba39420e8fd5
juzwaaaaaaaa <162950865+juzwaaaaaaaa@users.noreply.github.com>
2025-04-24 16:05:52 +0800

    update(25)

--- dfScan/pybackend/start.sh
+++ dfScan/pybackend/start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Install tesseract if not yet available
-apt-get update && apt-get install -y tesseract-ocr
-
-# Explicitly tell Python where to find tesseract
+# Set Tesseract path (Linux Render)
 export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata
 export PATH=$PATH:/usr/bin
 export TESSERACT_CMD=/usr/bin/tesseract
 
-# Run app with Gunicorn
+# Run the server
 exec gunicorn Scanner:app --bind 0.0.0.0:$PORT
